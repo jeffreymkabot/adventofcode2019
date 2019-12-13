81279fdc01046ddf72644eac2b62159cc993f4af
Jeffrey Kabot <jeffrey.m.kabot@gmail.com>
2019-12-13 13:23:08 -0500

    day 07 part 1

--- 05/1.ts
+++ 05/1.ts
@@ -1,127 +1,127 @@
 import * as fs from "fs";
 
+export interface Program {
+    instr: number[];
+    inputs: number[];
+}
+
+export interface Result {
+    memory: number[];
+    outputs: number[];
+}
+
 export interface Op {
     name: string;
     code: number;
     arity: number;
-    exec: (prog: Program, modes: (0 | 1)[], args: number[]) => void;
+    exec: (cpu: CPU, modes: (0 | 1)[], args: number[]) => void;
 }
 
-export class Program implements Iterable<null> {
-    public readonly memory: number[];
-    protected input: number;
-
-    public ptr = 0;
-    public done = false;
-    protected static ops: Op[] = [
-        {
-            name: "add",
-            code: 1,
-            arity: 3,
-            exec: (prog, modes, args) => {
-                const [a, b] = args.map((v, i) =>
-                    modes[i] ? v : prog.memory[v]
-                );
-                const c = args[2];
-                prog.memory[c] = a + b;
-                prog.ptr += 4;
-            }
-        },
-        {
-            name: "multiply",
-            code: 2,
-            arity: 3,
-            exec: (prog, modes, args) => {
-                const [a, b] = args.map((v, i) =>
-                    modes[i] ? v : prog.memory[v]
-                );
-                const c = args[2];
-                prog.memory[c] = a * b;
-                prog.ptr += 4;
-            }
-        },
-        {
-            name: "input",
-            code: 3,
-            arity: 1,
-            exec: (prog, modes, args) => {
-                const [a] = args;
-                prog.memory[a] = prog.input;
-                prog.ptr += 2;
-            }
-        },
-        {
-            name: "output",
-            code: 4,
-            arity: 1,
-            exec: (prog, modes, args) => {
-                const [a] = args;
-                console.log(prog.memory[a]);
-                prog.ptr += 2;
-            }
-        },
-        {
-            name: "halt",
-            code: 99,
-            arity: 0,
-            exec: prog => {
-                prog.done = true;
-            }
-        }
-    ];
-
-    constructor(instr: number[], input: number) {
-        this.memory = instr.slice();
-        this.input = input;
-    }
-
-    decodeNextOp(): () => void {
-        const [instr, ...parameters] = this.memory.slice(
-            this.ptr,
-            this.ptr + 4
-        );
-        const modes = [
-            Math.floor(instr / 100) % 10, // hundreds digit
-            Math.floor(instr / 1000) % 10, // thousands digit
-            Math.floor(instr / 1000) % 10 // ten thousands digit
-        ] as (0 | 1)[];
-
-        const opcode = instr % 100;
-        // https://github.com/Microsoft/TypeScript/issues/3841
-        const op = (this.constructor as typeof Program).ops.find(
-            op => op.code === opcode
-        );
-        if (!op) {
-            throw new Error(`Unknown opcode ${opcode}.`);
-        }
-
-        return () => op.exec(this, modes, parameters.slice(0, op.arity));
-    }
-    /**
-     * Execute program until a halt instruction is encountered.
-     */
-    exec() {
-        for (const _ of this) {
-        }
-    }
-    [Symbol.iterator]() {
-        return {
-            next: () => {
-                if (this.done) {
-                    return { done: this.done, value: null };
-                }
-                const opExec = this.decodeNextOp();
-                opExec();
-                return { done: this.done, value: null };
-            }
-        };
-    }
+export interface CPU {
+    ops: Op[];
+    memory: number[];
+    inputs: number[];
+    outputs: number[];
+    ptr: number;
+    halted: boolean;
 }
 
-export function getInput(): [number[], number] {
+export const ops: Op[] = [
+    {
+        name: "add",
+        code: 1,
+        arity: 3,
+        exec: (cpu, modes, args) => {
+            const [a, b] = args.map((v, i) => (modes[i] ? v : cpu.memory[v]));
+            const c = args[2];
+            cpu.memory[c] = a + b;
+            cpu.ptr += 4;
+        }
+    },
+    {
+        name: "multiply",
+        code: 2,
+        arity: 3,
+        exec: (cpu, modes, args) => {
+            const [a, b] = args.map((v, i) => (modes[i] ? v : cpu.memory[v]));
+            const c = args[2];
+            cpu.memory[c] = a * b;
+            cpu.ptr += 4;
+        }
+    },
+    {
+        name: "input",
+        code: 3,
+        arity: 1,
+        exec: (cpu, modes, args) => {
+            const [a] = args;
+            const inp = cpu.inputs.shift();
+            if (inp == null) {
+                throw new Error("Not enough inputs.");
+            }
+            cpu.memory[a] = inp;
+            cpu.ptr += 2;
+        }
+    },
+    {
+        name: "output",
+        code: 4,
+        arity: 1,
+        exec: (cpu, modes, args) => {
+            const [a] = args;
+            cpu.outputs.push(cpu.memory[a]);
+            cpu.ptr += 2;
+        }
+    },
+    {
+        name: "halt",
+        code: 99,
+        arity: 0,
+        exec: cpu => {
+            cpu.halted = true;
+        }
+    }
+];
+
+export function execute(ops: Op[], prog: Program): Result {
+    const cpu: CPU = {
+        ops,
+        memory: prog.instr.slice(),
+        inputs: prog.inputs.slice(),
+        outputs: [],
+        ptr: 0,
+        halted: false
+    };
+    while (!cpu.halted) {
+        const next = decodeNextOp(cpu);
+        next();
+    }
+    return {
+        memory: cpu.memory.slice(),
+        outputs: cpu.outputs.slice()
+    };
+}
+
+export function decodeNextOp(cpu: CPU) {
+    const [instr, ...params] = cpu.memory.slice(cpu.ptr, cpu.ptr + 4);
+    const modes = [
+        Math.floor(instr / 100) % 10, // hundreds digit
+        Math.floor(instr / 1000) % 10, // thousands digit
+        Math.floor(instr / 1000) % 10 // ten thousands digit
+    ] as (0 | 1)[];
+
+    const opcode = instr % 100;
+    const op = cpu.ops.find(op => op.code === opcode);
+    if (!op) {
+        throw new Error(`Unknown opcode ${opcode}.`);
+    }
+    return () => op.exec(cpu, modes, params.slice(0, op.arity));
+}
+
+export function getInput(): [number[], number[]] {
     let infile = process.argv[2] || "";
     if (!infile.includes(",")) {
-        infile = fs.readFileSync(__dirname + "/" + infile, {
+        infile = fs.readFileSync(infile, {
             encoding: "utf-8"
         });
     }
@@ -130,12 +130,12 @@ export function getInput(): [number[], number] {
         .split(",")
         .map(s => parseInt(s, 10))
         .filter(n => !isNaN(n));
-    const input = parseInt(process.argv[3], 10) || 0;
-    return [instr, input];
+    const inputs = process.argv.slice(3).map(s => parseInt(s, 10) || 0);
+    return [instr, inputs];
 }
 
 if (require.main === module) {
-    const [instr, input] = getInput();
-    const prog = new Program(instr, input);
-    prog.exec();
+    const [instr, inputs] = getInput();
+    const result = execute(ops, { instr, inputs });
+    console.log(...result.outputs);
 }
---
+++ 07/1.ts
@@ -0,0 +1,64 @@
+import { getInput, execute, Result } from "../05/1";
+import { ops2 } from "../05/2";
+
+export function permutations(seq: number[]): number[][] {
+    if (seq.length === 0) {
+        return [];
+    }
+    if (seq.length === 1) {
+        return [seq];
+    }
+
+    // Get permutations of the previous sequence (without first element).
+    // Insert first element at every position of each permutation of previous sequence
+    const [first, ...rest] = seq;
+    const perms = permutations(rest);
+
+    const results = [] as number[][];
+    for (let i = 0; i <= rest.length; i++) {
+        const insertAtIthPosition = (seq: number[]) => {
+            const copy = seq.slice();
+            copy.splice(i, 0, first);
+            return copy;
+        };
+        const x = perms.map(insertAtIthPosition);
+        results.push(...x);
+    }
+    return results;
+}
+
+export function series(instr: number[], combo: number[]): number {
+    let nextInput = 0;
+    for (let i = 0; i < combo.length; i++) {
+        let phaseSetting = combo[i];
+        const result = execute(ops2, {
+            instr,
+            inputs: [phaseSetting, nextInput]
+        });
+        nextInput = result.outputs[0] || 0;
+    }
+    return nextInput;
+}
+
+export function maxCombo(
+    instr: number[],
+    combos: number[][]
+): { max: number; maxCombo: number[] } {
+    let max = -Infinity;
+    let maxCombo = combos[0];
+    combos.forEach(c => {
+        const output = series(instr, c);
+        if (output > max) {
+            max = output;
+            maxCombo = c;
+        }
+    });
+    return { max, maxCombo };
+}
+
+if (require.main === module) {
+    const [instr] = getInput();
+    const combos = permutations([0, 1, 2, 3, 4]);
+    const result = maxCombo(instr, combos);
+    console.log(result.max, result.maxCombo);
+}
